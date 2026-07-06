c3788e07122435eab8321e5569ceee43f683f649
MEGHANADH AMBATI <meghanadhambati725@gmail.com>
2026-07-06 14:18:00 +0530

    main

---
+++ DSA/inventory/InventoryManager.java
@@ -0,0 +1,41 @@
+package DSA.inventory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InventoryManager {
+    private Map<String, Product> inventory = new HashMap<>();
+
+    public void addProduct(Product product) {
+        inventory.put(product.getProductId(), product);
+        System.out.println("Added: " + product);
+    }
+
+    public void updateProduct(String productId, int quantity, double price) {
+        Product product = inventory.get(productId);
+        if (product != null) {
+            product.setQuantity(quantity);
+            product.setPrice(price);
+            System.out.println("Updated: " + product);
+        } else {
+            System.out.println("Product not found!");
+        }
+    }
+
+    public void deleteProduct(String productId) {
+        Product removed = inventory.remove(productId);
+        if (removed != null) {
+            System.out.println("Deleted: " + removed);
+        } else {
+            System.out.println("Product not found!");
+        }
+    }
+
+    public void displayInventory() {
+        System.out.println("Current Inventory:");
+        for (Product product : inventory.values()) {
+            System.out.println(product);
+        }
+    }
+}
+
