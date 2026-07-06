72d4659f7003528759a7724d967587a41ec96b82
MEGHANADH AMBATI <meghanadhambati725@gmail.com>
2026-07-06 14:25:54 +0530

    main

---
+++ DSA/taskmanagement/Task.java
@@ -0,0 +1,24 @@
+package DSA.taskmanagement;
+
+public class Task {
+    private String taskId;
+    private String taskName;
+    private String status;
+
+    public Task(String taskId, String taskName, String status) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.status = status;
+    }
+
+    public String getTaskId() { return taskId; }
+    public String getTaskName() { return taskName; }
+    public String getStatus() { return status; }
+
+    public void setStatus(String status) { this.status = status; }
+
+    @Override
+    public String toString() {
+        return "Task [ID=" + taskId + ", Name=" + taskName + ", Status=" + status + "]";
+    }
+}
